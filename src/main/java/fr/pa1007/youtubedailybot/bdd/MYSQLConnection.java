c5d05f6a17ad61c4aa1ace372b12d72ffc0842d0
pa1007 <pa.fourriere@sfr.fr>
2019-06-20 16:17:40 +0200

    BDD main class

---
+++ src/main/java/fr/pa1007/youtubedailybot/bdd/MYSQLConnection.java
@@ -0,0 +1,144 @@
+package fr.pa1007.youtubedailybot.bdd;
+
+import fr.pa1007.youtubedailybot.statistics.Stats;
+import java.sql.*;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MYSQLConnection {
+
+    public Connection connection;
+    public boolean    connected;
+
+
+    public MYSQLConnection() {
+    }
+
+    public Map<String, Stats> getAllInfos() throws SQLException, ClassNotFoundException {
+        testIfConnectionOpened();
+        Map<String, Stats> infos = new HashMap<>();
+        PreparedStatement  p     = connection.prepareStatement(Constants.GET_ALL_STATS);
+        p.setInt(1, Calendar.getInstance().get(Calendar.MONTH) + 1);
+        ResultSet rs = p.executeQuery();
+        rs.beforeFirst();
+        while (rs.next()) {
+            PreparedStatement p2 = connection.prepareStatement(Constants.GET_CHAN_NAME);
+            p2.setString(1, rs.getString(1));
+            ResultSet rs2 = p2.executeQuery();
+            rs2.first();
+            Stats s = new Stats(
+                    rs2.getString(1),
+                    rs.getString(1),
+                    rs.getLong(2),
+                    rs.getLong(3),
+                    rs.getLong(4),
+                    rs.getLong(5),
+                    rs.getLong(6)
+            );
+            infos.put(rs.getString(1), s);
+        }
+        p.close();
+        return infos;
+    }
+
+    public void addStat(Stats s) throws SQLException, ClassNotFoundException {
+        testIfConnectionOpened();
+        if (!isInBase(s)) {
+            PreparedStatement p = connection.prepareStatement(Constants.START_STATS);
+            p.setString(1, s.getChanID());
+            p.setLong(2, s.getCumulatedViews());
+            p.setLong(3, s.getCumulatedLikes());
+            p.setLong(4, s.getCumulatedComms());
+            p.setLong(5, s.getCumulatedDisLikes());
+            p.setInt(6, Calendar.getInstance().get(Calendar.MONTH) + 1);
+            p.execute();
+            if (!nameInBase(s.getChanID(), s.getChanName())) {
+                addName(s);
+            }
+            p.close();
+        }
+        else {
+            sumStats(s);
+        }
+    }
+
+    public boolean isInBase(Stats s) throws SQLException, ClassNotFoundException {
+        testIfConnectionOpened();
+        PreparedStatement p = connection.prepareStatement(Constants.EXIST_STATS);
+        p.setString(1, s.getChanID());
+        p.setInt(2, Calendar.getInstance().get(Calendar.MONTH) + 1);
+        ResultSet rs = p.executeQuery();
+        rs.first();
+        return rs.getBoolean(1);
+    }
+
+    /**
+     * to get a new Connection
+     *
+     * @return Connection
+     * @throws SQLException           if the program is unable to connect to the database
+     * @throws ClassNotFoundException if the jdbc driver is not present
+     */
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Class.forName("com.mysql.jdbc.Driver");
+        if (connected && connection.isValid(1)) {
+
+            return connection;
+        }
+        else {
+            connection = DriverManager.getConnection(Constants.URL, Constants.USER_NAME, Constants.USER_PASSWORD);
+            connected = true;
+            return connection;
+        }
+
+    }
+
+
+    public void addName(Stats s) throws SQLException, ClassNotFoundException {
+        addName(s.getChanID(), s.getChanName());
+    }
+
+    public void addName(String chanID, String chanName) throws SQLException, ClassNotFoundException {
+        testIfConnectionOpened();
+        if (!nameInBase(chanID, chanName)) {
+            PreparedStatement p = connection.prepareStatement(Constants.ADD_NAME);
+            p.setString(1, chanID);
+            p.setString(2, chanName);
+            p.execute();
+            p.close();
+        }
+    }
+
+    public boolean nameInBase(String chanID, String chanName) throws SQLException, ClassNotFoundException {
+        testIfConnectionOpened();
+        PreparedStatement p = connection.prepareStatement(Constants.TEST_NAME);
+        p.setString(1, chanID);
+        p.setString(2, chanName);
+        ResultSet rs = p.executeQuery();
+        rs.first();
+        return rs.getBoolean(1);
+    }
+
+
+    public void sumStats(Stats s) throws SQLException, ClassNotFoundException {
+        testIfConnectionOpened();
+        PreparedStatement p = connection.prepareStatement(Constants.UPDATE_STATS);
+        p.setLong(1, s.getCumulatedViews());
+        p.setLong(2, s.getCumulatedViews());
+        p.setLong(3, s.getCumulatedDisLikes());
+        p.setLong(4, s.getCumulatedComms());
+        p.setLong(5, s.getNumber());
+        p.setString(6, s.getChanID());
+        p.setInt(7, Calendar.getInstance().get(Calendar.MONTH) + 1);
+        p.execute();
+        p.close();
+    }
+
+    public void testIfConnectionOpened() throws SQLException, ClassNotFoundException {
+        if (!connected) {
+            getConnection();
+        }
+    }
+}
